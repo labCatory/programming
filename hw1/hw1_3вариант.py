e98a7e1ffe52758038baa4b42741cd0113aeb3b7
labCatory <evchelapchiy@edu.hse.ru>
2017-09-24 15:32:23 +0300

    дз

---
+++ hw1/hw1_3вариант.py
@@ -0,0 +1,12 @@
+#3 вариант
+a=int(input())
+b=int(input())
+c=int(input())
+if a%b==c:
+	print('condition 3 - Y')
+else :
+	print ('condition 3 - N')
+if -b/a==c:
+	print('condition 4 - Y')
+else :
+	print ('condition 4 - N')
